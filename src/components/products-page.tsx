81bac4ed21870a6ca2ad03ea181da5853ee26ce3
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2024-09-13 13:12:31 +0600

    type fix

--- src/components/products-page.tsx
+++ src/components/products-page.tsx
@@ -18,7 +18,7 @@ const ProductsPage = () => {
   }
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {products?.map((product) => (
+      {products?.map((product: Product) => (
         <ProductCard key={product.id} product={product} addToCart={addToCart} />
       ))}
     </div>
